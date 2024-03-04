4af9c5ee5849902fcb75df5ef04829de4f01a879
Daniel Osorio <ndmd@997@gmail.com>
2024-03-03 20:58:24 -0500

    se agrega la foto de perfil

--- src/components/DescripcionProfile/DescripcionProfile.jsx
+++ src/components/DescripcionProfile/DescripcionProfile.jsx
@@ -5,7 +5,7 @@ export const DescripcionProfile = () => {
     <>
     <div class="info">
 
-    <p class="descripcion">Yo Daniel Osorio Escobar me considero una persona puntual, responsable, amable  respetuoso y se trabajar en equipo, actualmente soy aprendiz SENA de la titulación de análisis y desarrollo de software con conocimientos básicos en Java, JavaScript , CSS y lenguajes de SQL, me gusta el futbol el voleibol , escuchar música, decidí estudiar esta formación por q me llama la atención la programación y me gusta, mis intereses a nivel profesional es salir adelante aprender cada día mas y poder lograr todo lo que me e propuesto.</p>
+    <p class="descripcion">Yo Daniel Osorio Escobar me considero una persona puntual, responsable, amable  respetuoso y se trabajar en equipo, actualmente soy aprendiz SENA de la titulación de análisis y desarrollo de software con conocimientos básicos en Java, JavaScript , CSS, lenguajes de SQL y MongoDB, me gusta el futbol el voleibol , escuchar música, decidí estudiar esta formación por q me llama la atención la programación y me gusta, mis intereses a nivel profesional es salir adelante aprender cada día mas y poder lograr todo lo que me e propuesto.</p>
     
     </div>
     
